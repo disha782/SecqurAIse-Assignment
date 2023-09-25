4288dd1dcff89f60eae4e55a824deda1886158c1
disha782 <dishapoojari28820@gmail.com>
2023-09-25 20:09:31 +0530

    Second Commit

--- app/src/main/java/com/example/secquraise/ChargingReceiver.java
+++ app/src/main/java/com/example/secquraise/ChargingReceiver.java
@@ -1,10 +1,17 @@
 package com.example.secquraise;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.NotificationCompat;
 
 public class ChargingReceiver extends BroadcastReceiver {
     private TextView battery_charge_status, battery_per;
@@ -15,19 +22,39 @@ public class ChargingReceiver extends BroadcastReceiver {
         boolean isCharging  = status == BatteryManager.BATTERY_STATUS_CHARGING
                 || status == BatteryManager.BATTERY_STATUS_FULL;
         if (isCharging){
-            battery_charge_status.setText("ON");
+            battery_charge_status.setText(R.string.on_status);
         }else {
-            battery_charge_status.setText("OFF");
+            battery_charge_status.setText(R.string.off_status);
         }
 
         int batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
         int batteryScale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
         int batteryPercent = (batteryLevel*100)/batteryScale;
         battery_per.setText(batteryPercent+"%");
+        if(batteryPercent <= 20){
+            sendNotification(context, "Low Battery", "Battery is below 20%");
+        }
     }
 
     public ChargingReceiver(TextView battery_charge_status, TextView battery_per){
         this.battery_charge_status = battery_charge_status;
         this.battery_per = battery_per;
+    }
+
+    private void sendNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("channel_id", "Channel Name", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "channel_id")
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(1, builder.build());
     }
 }
--- app/src/main/java/com/example/secquraise/MainActivity.java
+++ app/src/main/java/com/example/secquraise/MainActivity.java
@@ -8,10 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -20,22 +22,38 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.Manifest;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView time, capture_val, frequency_val, connectivity_val, charging_val_bool, charge_val, location_val;
+    Button refresh_btn;
     ChargingReceiver chargingReceiver;
     private static final int LOCATION_REQUEST_CODE = 1001;
+    private static final long RETRY_DELAY_MS = 5000;
     int freqency = 15;
+    int count = 1;
+    private static final int MAX_RETRY_COUNT = 3;
+    private int retryCount = 0;
+    DatabaseReference dbref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,17 +63,104 @@ public class MainActivity extends AppCompatActivity {
         requestLocationPermission();
         connect_values();
         dateTime();
-
+        setLocation_val();
         chargingReceiver = new ChargingReceiver(charging_val_bool, charge_val);
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         registerReceiver(chargingReceiver, filter);
-
+        refreshValues();
         frequency_val.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeFrequency();
             }
         });
+        refresh_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshValues();
+            }
+        });
+
+        schedulePeriodicUpdate();
+    }
+    private void schedulePeriodicUpdate() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                refreshValues();
+                handler.postDelayed(this, (long) freqency*60*1000);
+            }
+        }, (long) freqency*60*1000);
+    }
+
+    private void refreshValues() {
+        connect_values();
+        dateTime();
+        setLocation_val();
+        count++;
+        capture_val.setText(String.valueOf(count));
+        chargingReceiver = new ChargingReceiver(charging_val_bool, charge_val);
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(chargingReceiver, filter);
+        if (!isNetworkConnected()){
+            storeDataLocally();
+            if (retryCount < MAX_RETRY_COUNT){
+                retryCount++;
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshValues();;
+                    }
+                }, RETRY_DELAY_MS);
+                return;
+            }
+        }
+        retryCount = 0;
+
+        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+
+        String internetConnectivity = connectivity_val.getText().toString();
+        String batteryStatus = charging_val_bool.getText().toString();
+        String batteryPercentage = charge_val.getText().toString();
+        String location = location_val.getText().toString();
+        String timestamp = time.getText().toString();
+
+        CaptureData capturedData = new CaptureData(internetConnectivity, batteryStatus, batteryPercentage, location, timestamp);
+
+        Call<ApiResponse> call = apiService.captureData(capturedData);
+        call.enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.i("MainActivity", "Data stored successfully");
+                } else {
+                    Log.e("MainActivity", "Error storing data: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                Log.e("MainActivity", "API call failed: " + t.getMessage());
+            }
+        });
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                dbref = FirebaseDatabase.getInstance("https://secquraise-ceb6d-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("user_details");
+                String userId = dbref.push().getKey();
+                DatabaseReference userRef = dbref.child(userId);
+                userRef.child("Internet Connectivity Status").setValue(connectivity_val.getText().toString());
+                userRef.child("Battery Charging status").setValue(charging_val_bool.getText().toString());
+                userRef.child("Battery Charge Percentage").setValue(charge_val.getText().toString());
+                userRef.child("Location").setValue(location_val.getText().toString());
+                userRef.child("Timestamp").setValue(time.getText().toString());
+            }
+        }, RETRY_DELAY_MS);
+
     }
 
     private void changeFrequency(){
@@ -73,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
                 String value = input.getText().toString();
                 if (!value.isEmpty()){
                     freqency = Integer.parseInt(value);
-                    frequency_val.setText(freqency+"");
+                    frequency_val.setText(String.valueOf(freqency));
                 }
             }
         });
@@ -94,7 +199,9 @@ public class MainActivity extends AppCompatActivity {
         charging_val_bool = findViewById(R.id.batterychargeactionval);
         charge_val = findViewById(R.id.batterychargeperval);
         location_val = findViewById(R.id.locationvalue);
+        refresh_btn = findViewById(R.id.refreshbtn);
         frequency_val.setText("15");
+        capture_val.setText(String.valueOf(count));
     }
 
     public void connect_values() {
@@ -102,9 +209,9 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
         if (isConnected) {
-            connectivity_val.setText("ON");
+            connectivity_val.setText(R.string.on_status);
         } else {
-            connectivity_val.setText("OFF");
+            connectivity_val.setText(R.string.off_status);
         }
     }
 
@@ -142,7 +249,6 @@ public class MainActivity extends AppCompatActivity {
     public void setLocation_val() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
@@ -169,9 +275,58 @@ public class MainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
+    public boolean isNetworkConnected(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    private void storeDataLocally() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LocalData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("InternetConnectivity", connectivity_val.getText().toString());
+        editor.putString("BatteryChargingStatus", charging_val_bool.getText().toString());
+        editor.putString("BatteryChargePercentage", charge_val.getText().toString());
+        editor.putString("Location", location_val.getText().toString());
+        editor.putString("Timestamp", time.getText().toString());
+
+        editor.apply();
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            restoreLocalData();
+        }
+    };
+
+    private void restoreLocalData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("LocalData", MODE_PRIVATE);
+        connectivity_val.setText(sharedPreferences.getString("InternetConnectivity", ""));
+        charging_val_bool.setText(sharedPreferences.getString("BatteryChargingStatus", ""));
+        charge_val.setText(sharedPreferences.getString("BatteryChargePercentage", ""));
+        location_val.setText(sharedPreferences.getString("Location", ""));
+        time.setText(sharedPreferences.getString("Timestamp", ""));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(Intent.ACTION_BOOT_COMPLETED));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(chargingReceiver);
+        count = 0;
+        retryCount = 0;
     }
 }
